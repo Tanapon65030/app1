c3722561d78c40cc4806a867afece387242630ed
Tanapon65030 <152829114+Tanapon65030@users.noreply.github.com>
2023-12-25 11:10:46 +0700

    Update App.js reteurn
    Update App.js reteurn

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
 function App() {
-  let r = '<b>React</b>'
-  let rn = 'React Native'
-  const el = (
-    <div>
-      Create Web & Mobile Apps<br/>
-      with <span dangerouslySetInnerHTML={{__html: r}}/> & {rn}
-    </div>
-  )
-  return el
+  return (
+    <table border="1">
+    <tr><th>Product</th><th>Price</th></tr>
+    <tr><td>React</td><td>320</td></tr>
+    <tr><td>React Native</td><td>340</td></tr>
+    </table>
+    )
 }
 
 export default App;
