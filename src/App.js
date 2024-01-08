67a8c26892458dbb96d1f8bcf0567fda2d05d0df
Tanapon65030 <152829114+Tanapon65030@users.noreply.github.com>
2024-01-08 11:31:14 +0700

    add class-components.js
    add class-components.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import {Header,Content,Footer} from './func-components'
+import Calendar from './class-components'
 
 function App() {
   return (
     <>
       <Header/>
+      <p><center><Calendar/></center></p>
       <Content/>
       <Footer/>
     </>
